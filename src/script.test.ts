6071436aca58fc2d56cfd16f298f3d464a329ef1
Hector Alvarez <alvarez.hectorm@outlook.com>
2025-05-13 23:24:35 -0500

    feat: setup testing environment with Jest

---
+++ src/script.test.ts
@@ -0,0 +1,7 @@
+// Placeholder test file for src/script.ts
+describe('script.ts', () => {
+  test('should be implemented', () => {
+    // TODO: Add actual tests for clock logic
+    expect(true).toBe(true);
+  });
+});
